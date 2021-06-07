b6dd39f6e82275c712cd93d93581df7bb7df764e
Sam Belliveau <sam.belliveau@gmail.com>
2021-06-06 22:39:30 -0400

    added threading

--- src/constants.hpp
+++ src/constants.hpp
@@ -4,10 +4,10 @@
 namespace sb {
     const int MAX_HITS = 3;
     const int MAX_MARCH_ITER = 96;
-    const int MAX_MARCH_ITER_LIGHTING = 96;
+    const int MAX_MARCH_ITER_LIGHTING = 64;
 
-    const double EPS = 1e-3;
-    const double FIX_EPS = 1e-2;
+    const double EPS = 1e-2;
+    const double FIX_EPS = 2e-2;
 
     const double MAX_STEP = 64;
     
